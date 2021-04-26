b2b8f17dc8793847fbd27c77929740e99c771dc6
Yatharth Sharma <yatharth0045@gmail.com>
2021-04-26 15:33:31 +0530

    Added demo1

---
+++ demo1/main.tf
@@ -0,0 +1,96 @@
+## VARIABLES
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "private_key_path" {}
+variable "key_name" {}
+variable "region" {
+  default = "us-east-1"
+}
+
+## PROVIDERS
+
+provider "aws" {
+  access_key    = var.aws_access_key
+  secret_key    = var.aws_secret_key
+  region        = var.region
+}
+
+## DATA
+
+data "aws_ami" "aws-linux" {
+  most_recent = true
+  owners = [ "amazon" ]
+  filter {
+    name    = "name"
+    values = [ "amzn-ami-hvm*" ]
+  }
+  filter {
+    name    = "root-device-type"
+    values = [ "ebs" ]
+  }
+  filter {
+    name    = "virtualization-type"
+    values = [ "hvm" ]
+  }
+}
+
+## RESOURCES
+
+resource "aws_default_vpc" "default" {
+  
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name = "nginx_demo"
+  description = "Allow ports for nginx demo"
+  vpc_id = aws_default_vpc.default.id
+  
+  ingress {
+    cidr_blocks = [ "0.0.0.0/0" ]
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+  }
+
+  ingress {
+    cidr_blocks = [ "0.0.0.0/0" ]
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+  }
+
+  egress {
+    cidr_blocks = [ "0.0.0.0/0" ]
+    from_port = 0
+    protocol = -1
+    to_port = 0
+  }
+}
+
+resource "aws_instance" "nginx" {
+  ami = data.aws_ami.aws-linux.id
+  instance_type = "t2.micro"
+  key_name = var.key_name
+  vpc_security_group_ids = [ aws_security_group.allow_ssh.id ]
+
+  connection {
+    type    = "ssh"
+    host    = self.public_ip
+    user    = "ec2-user"
+    private_key = file(var.private_key_path)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+        "sudo yum install nginx -y",
+        "sudo service nginx start"
+    ]
+  }
+}
+
+## OUTPUT
+
+output "aws_instance_public_dns" {
+  value = aws_instance.nginx.public_dns
+}
