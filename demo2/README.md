ccc4d1702a01ba7adafda05a7f684b5551979ae2
Yatharth Sharma <yatharth0045@gmail.com>
2021-04-26 15:33:41 +0530

    Added demo2

---
+++ demo2/README.md
@@ -0,0 +1,16 @@
+# Demo 2
+
+```
+cd demo2
+
+terraform version
+## Output: Terraform v0.12.19
+
+terraform init
+
+terraform plan -out file.tfplan
+
+terraform apply "file.tfplan"
+
+terraform destroy
+```
---
+++ demo2/main.tf
@@ -0,0 +1,133 @@
+## VARIABLES
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "private_key_path" {}
+variable "key_name" {}
+variable "region" {
+  default = "us-east-1"
+}
+variable "network_address_space" {
+  default = "10.1.0.0/16"
+}
+variable "subnet1_address_space" {
+  default = "10.1.0.0/24"
+}
+
+## PROVIDERS
+
+provider "aws" {
+  access_key    = var.aws_access_key
+  secret_key    = var.aws_secret_key
+  region        = var.region
+}
+
+## DATA
+
+data "aws_availability_zones" "available" {}
+
+data "aws_ami" "aws-linux" {
+  most_recent = true
+  owners = [ "amazon" ]
+  filter {
+    name    = "name"
+    values = [ "amzn-ami-hvm*" ]
+  }
+  filter {
+    name    = "root-device-type"
+    values = [ "ebs" ]
+  }
+  filter {
+    name    = "virtualization-type"
+    values = [ "hvm" ]
+  }
+}
+
+## RESOURCES
+
+resource "aws_vpc" "vpc" {
+  cidr_block            = var.network_address_space
+  enable_dns_hostnames  = "true"
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id    =   aws_vpc.vpc.id
+}
+
+resource "aws_subnet" "subnet1" {
+  cidr_block = var.subnet1_address_space
+  vpc_id     = aws_vpc.vpc.id
+  map_public_ip_on_launch = "true"
+  availability_zone = data.aws_availability_zones.available.names[0]
+}
+
+resource "aws_route_table" "rtb" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "rta-subnet1" {
+  subnet_id = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.rtb.id
+}
+
+resource "aws_security_group" "nginx-sg" {
+  name = "nginx_sg"
+  description = "Allow ports for nginx"
+  vpc_id = aws_vpc.vpc.id
+  
+  ingress {
+    cidr_blocks = [ "0.0.0.0/0" ]
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+  }
+
+  ingress {
+    cidr_blocks = [ "0.0.0.0/0" ]
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+  }
+
+  egress {
+    cidr_blocks = [ "0.0.0.0/0" ]
+    from_port = 0
+    protocol = -1
+    to_port = 0
+  } 
+}
+
+resource "aws_instance" "nginx" {
+  ami = data.aws_ami.aws-linux.id
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.subnet1.id
+  key_name = var.key_name
+  vpc_security_group_ids = [ aws_security_group.nginx-sg.id ]
+
+  connection {
+    type    = "ssh"
+    host    = self.public_ip
+    user    = "ec2-user"
+    private_key = file(var.private_key_path)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+        "sudo yum install nginx -y",
+        "sudo service nginx start",
+        "sudo rm /usr/share/nginx/html/index.html",
+        "echo '<html><head><title>Blue Team Server</title></head><body style=\"background-color:#1F778D\"><p style=\"text-align: center;\"><span style=\"color:#FFFFFF;\"><span style=\"font-size:28px;\">Blue Team</span></span></p></body></html>' | sudo tee /usr/share/nginx/html/index.html"
+    ]
+  }
+}
+
+## OUTPUT
+
+output "aws_instance_public_dns" {
+  value = aws_instance.nginx.public_dns
+}
