8e03720c4a524ac72df39c41c6f72478401d92f8
Yatharth Sharma <yatharth0045@gmail.com>
2021-05-09 10:35:24 +0530

    Updated demo1 files

--- demo1/main.tf
+++ demo1/main.tf
@@ -1,40 +1,3 @@
-## VARIABLES
-
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "private_key_path" {}
-variable "key_name" {}
-variable "region" {
-  default = "us-east-1"
-}
-
-## PROVIDERS
-
-provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = var.region
-}
-
-## DATA
-
-data "aws_ami" "aws-linux" {
-  most_recent = true
-  owners      = ["amazon"]
-  filter {
-    name   = "name"
-    values = ["amzn-ami-hvm*"]
-  }
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
 ## RESOURCES
 
 resource "aws_default_vpc" "default" {
@@ -87,10 +50,4 @@ resource "aws_instance" "nginx" {
       "sudo service nginx start"
     ]
   }
-}
-
-## OUTPUT
-
-output "aws_instance_public_dns" {
-  value = aws_instance.nginx.public_dns
 }
---
+++ demo1/vars.tf
@@ -0,0 +1,9 @@
+## VARIABLES
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "private_key_path" {}
+variable "key_name" {}
+variable "region" {
+  default = "us-east-1"
+}
